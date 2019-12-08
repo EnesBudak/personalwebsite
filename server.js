e17e28c6609ea325b934739c8809d32370b3c798
EnesBudak <45215087+EnesBudak@users.noreply.github.com>
2019-12-09 00:15:05 +0300

    5

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 
-
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(__dirname + "/dist/"));
+app.use(express.static(__dirname + "/dist/"));
 
 app.get(/.*/, (req, res) => {
   res.sendfile(__dirname + "/dist/index.html");
 });
-};
+
 
 
 app.listen(port);
